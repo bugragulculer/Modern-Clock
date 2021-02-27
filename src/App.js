2b52be2d01795b918db9d42e671c6d53bb303e07
Buğrahan Gülcüler <78660618+bugragulculer@users.noreply.github.com>
2021-02-27 21:48:53 +0300

    First Version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
+// Column Function: Takes two inputs as max value and current value, maps them in arrays. 
+function Column({max, current}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      className='column'
+      data-id={current}
+      // Style part creates the animation of the current value. Change the below number -30 with the column_height value in App.css file. (make it equal to each other)
+      // It takes the current value and multiplies with the -30 value.
+      style={{ top: `${-30 * current}px` }}
+    >
+      {Array(max + 1)
+        .fill("")
+        .map((_e, i) => (
+          <div 
+            key={i}
+            className={`number${current === i.toString() ? " active": ""}`}
+          >
+            {i}
+          </div>
+        ))}
+      </div>
   );
 }
 
-export default App;
+function splitNum(num) {
+  return ('0' + num.toString()).slice(-2).split('');
+}
+
+export default function App() {
+  const [date, setDate] = useState(new Date());
+
+  const s = splitNum(date.getSeconds()),
+    m = splitNum(date.getMinutes()),
+    h = splitNum(date.getHours());
+  
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+  }, []);
+
+  return (
+    <div className='App'>
+      <Column max={2} current={h[0]} />
+      <Column max={9} current={h[1]} />
+      <p className='spacer' >:</p>
+      <Column max={5} current={m[0]} />
+      <Column max={9} current={m[1]} />
+      <p className='spacer' >:</p>
+      <Column max={5} current={s[0]} />
+      <Column max={9} current={s[1]} />
+    </div>
+  );
+}
